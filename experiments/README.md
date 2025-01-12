00b6c17e197f065259c51535e8900a7920ff72a9
Daniel Tan <dtch1997@users.noreply.github.com>
2025-01-12 10:03:07 -0800

    update

--- experiments/README.md
+++ experiments/README.md
@@ -2,13 +2,21 @@
 
 Here we investigate whether we can use SAE feature steering to improve the performance of a model on a task.
 
-1. We consider the MMLU 0-shot multiiple choice task.
+1. We consider the MMLU multiiple choice task.
 2. We evaluate Llama 3.1 8B and Llama 3.3 70B instruction-tuned models on the task.
 3. For each model, we compare the performance of the model before and after steering.
 
 We use the [GoodFire Ember API](https://goodfire.ai) to get the SAE features for a model.
 We use the auto-steering API to automatically select SAE features based on the prompt. 
 We use the [Inspect AI](https://inspect.ai-safety-institute.org.uk/) library to evaluate the performance of the model on a task.
+
+## Steering Protocols
+
+1. `zero-shot`: Do nothing, i.e. this is equivalent to running the original model. 
+2. `zero-shot-steering`: Provide a natural language description of the task, and use the Auto-Steering API to find relevant SAE features.
+3. `few-shot-prompting`: No steering, but give the model few-shot examples when solving the task.
+4. `few-shot-steering`: Give the ICL examples to the Auto-Steering API, which selects SAE features based on the ICL examples.
+5. `few-shot-prompting-steering`: ICL examples used for both prompting and steering.
 
 ## Results
 
--- experiments/main.py
+++ experiments/main.py
@@ -6,7 +6,9 @@ from dotenv import load_dotenv
 
 from goodfire import Client, Variant
 from inspect_ai import eval_set, Task
-from inspect_evals.mmlu.mmlu import mmlu_0_shot
+
+from inspect_ai.dataset import Sample
+from inspect_evals.mmlu.mmlu import mmlu_0_shot, mmlu_5_shot
 
 import test_time_sae_steering.ember # noqa: F401
 from test_time_sae_steering.ember.controller import write_controller_params
@@ -17,30 +19,34 @@ project_dir = curr_dir.parent
 load_dotenv(project_dir / ".env")
 GOODFIRE_API_KEY = os.getenv("GOODFIRE_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+client = Client(api_key=GOODFIRE_API_KEY)
 
 models = [
     "meta-llama/Meta-Llama-3.1-8B-Instruct",
     "meta-llama/Llama-3.3-70B-Instruct",
 ]
-
-short_names = {
-    "meta-llama/Meta-Llama-3.1-8B-Instruct": "llama31_8b",
-    "meta-llama/Llama-3.3-70B-Instruct": "llama33_70b",
-}
-
 # Limit in order to avoid rate limits
 limit = 50
+
+short_names = {
+    "meta-llama/Meta-Llama-3.1-8B-Instruct": "llama31_8b_it",
+    "meta-llama/Llama-3.3-70B-Instruct": "llama33_70b_it",
+}
 
 def evaluate_variant(
     task: Task,
     variant: Variant,
     suffix: str = "default",
-    limit: int = 50,
+    limit: int = limit,
+    results_dir: str | Path = curr_dir / "results",
 ) -> None:
+    task_name = task.name.replace("inspect_evals/", "")
     controller_params = variant.controller.json()
     # Set the controller params
     write_controller_params(controller_params)
-    log_dir = (curr_dir / f"logs/mmlu_0_shot/{suffix}_{short_names[variant.base_model]}").absolute()
+    log_dir = (results_dir / task_name / suffix / short_names[variant.base_model]).absolute()
+    # NOTE: we use eval set because it skips the run if previous results have been completed
+    # This saves on API requests
     eval_set(
         model = f"ember/{variant.base_model}",
         tasks = [task],
@@ -55,27 +61,60 @@ def evaluate_variant(
             "suffix": suffix,
             "limit": limit,
         }, f)
-    
+
+def zero_shot(base_model):
+    return Variant(base_model = base_model)
+
+def zero_shot_steering(base_model, description: str):
+    variant = Variant(base_model = base_model)
+    edits = client.features.AutoSteer(
+        specification=description,  # Natural language description
+        model=variant,  # Model variant to use
+    )
+    variant.set(edits)
+    return variant
+
+def few_shot_steering(base_model, examples: list[str]):
+    variant = Variant(base_model = base_model)
+    edits = client.features.AutoSteer(
+        specification=examples,  # Natural language description
+        model=variant,  # Model variant to use
+    )
+    variant.set(edits)
+    return variant
+
+def get_few_shot_examples(task: Task) -> list[Sample]:
+    # TODO: check we don't leak any few-shot examples
+    return task.dataset.samples[:5]
 
 if __name__ == "__main__":
 
-    # Set up the task
-    task = mmlu_0_shot(subjects=["high_school_mathematics"])
-
     for model in models:
 
-        variant = Variant(base_model = model)
+        # Zero-shot prompting
+        task = mmlu_0_shot(subjects=["high_school_mathematics"])
 
-        # Evaluate the base model on the task
-        evaluate_variant(task, variant, suffix = "default", limit = limit)
+        # 1. Zero-shot
+        variant = zero_shot(model)
+        evaluate_variant(task, variant, suffix = "zero-shot", limit = limit)
 
-        # Get the auto-steering edits
-        client = Client(api_key=GOODFIRE_API_KEY)
-        edits = client.features.AutoSteer(
-            specification="Solve a high school mathematics question",  # Natural language description
-            model=variant,  # Model variant to use
-        )
-        variant.set(edits)
+        # 2. Zero-shot steering
+        variant = zero_shot_steering(model, "Solve a high school mathematics question")
+        evaluate_variant(task, variant, suffix = "zero-shot-steering", limit = limit)
 
-        # Evaluate the autosteered model
-        evaluate_variant(task, variant, suffix = "autosteered", limit = limit)
+        # 3. Few-shot steering
+        few_shot_examples = get_few_shot_examples(task)
+        variant = few_shot_steering(model, few_shot_examples)
+        evaluate_variant(task, variant, suffix = "few-shot-steering", limit = limit)
+
+        # 4. Few-shot prompting
+        # Few-shot prompting
+        task = mmlu_5_shot(subjects=["high_school_mathematics"])
+        variant = zero_shot(model)
+        evaluate_variant(task, variant, suffix = "few-shot-prompting", limit = limit)
+
+        # 5. Few-shot prompting and steering
+        # Few-shot steering 
+        variant = few_shot_steering(model, few_shot_examples)
+        evaluate_variant(task, variant, suffix = "few-shot-prompting-steering", limit = limit)
+
